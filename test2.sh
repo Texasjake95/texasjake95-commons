b735ebc00dee98ec2cc6312503f1d2c92bea7759
Texasjake95 <texasjake95@gmail.com>
2014-03-05 18:36:42 -0600

    Attempt 3?

--- test2.sh
+++ test2.sh
@@ -4,8 +4,8 @@ rm -rf maven-test
 git clone https://Texasjake95@github.com/Texasjake95/maven-repo.git ./maven-test -v
 ./gradlew build uploadArchives
 cd ./maven-test
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+git remote rm origin
+git remote add origin https://Texasjake95:${GH_TOKEN}@github.com/Texasjake95/maven-repo.git
 git config user.email "texasjake95@gmail.com"
 git config user.name "Texasjake95"
 git add .
