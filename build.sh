96bb7eb1639eb15f586550e26ec71211b1fb92bd
Texasjake95 <texasjake95@gmail.com>
2014-03-06 09:13:50 -0600

    Commented build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
-rm -rf maven-test
+# delete the directory form last time in case of failure
+rm -rf maven-repo
+
+# pull the maven repo from git
 git clone https://Texasjake95@github.com/Texasjake95/maven-repo.git ./maven-test -v
+
+# run gradle
 ./gradlew build uploadArchives
-cd ./maven-test
+
+# change to the maven reop directory
+cd ./maven-repo
+
+# add correct origin to the repo 
 git remote rm origin
 git remote add origin https://Texasjake95:${GH_TOKEN}@github.com/Texasjake95/maven-repo.git
+
+# configure repo
 git config user.email "texasjake95@gmail.com"
 git config user.name "Texasjake95"
+
+# configure git push
 git config push.default current
+
+# preform commit
 git add .
 git commit -q -m "Travis-CI Build Push"
+
+# push commit
 git push -q
-rm -rf maven-test
+
+# delete repo since we are done with it
+rm -rf maven-repo
