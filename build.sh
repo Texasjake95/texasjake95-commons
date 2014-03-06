e249ef543447efe14c4e948eaa82b7f36dad6760
Texasjake95 <texasjake95@gmail.com>
2014-03-06 08:53:17 -0600

    Trying to get rid of error messages

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ git remote rm origin
 git remote add origin https://Texasjake95:${GH_TOKEN}@github.com/Texasjake95/maven-repo.git
 git config user.email "texasjake95@gmail.com"
 git config user.name "Texasjake95"
+git config push.default current
 git add .
 git commit -q -m "Travis-CI Build Push"
 git push -q
