ee0ba7d99897388c396c2db3cb973536079b0371
Texasjake95 <texasjake95@gmail.com>
2014-03-05 18:31:22 -0600

    Ok maybe not

--- test2.sh
+++ test2.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
 rm -rf maven-test
 git clone https://Texasjake95@github.com/Texasjake95/maven-repo.git ./maven-test -v
 ./gradlew build uploadArchives
 cd ./maven-test
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+git config --global user.name "Texasjake95"
 git add .
 git commit -m "Travis-CI Build Push"
 git push 
