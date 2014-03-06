182c28aabdf504398a83848fbfeff39792dceccd
Texasjake95 <texasjake95@gmail.com>
2014-03-05 18:17:31 -0600

    Attempt 2

--- test2.sh
+++ test2.sh
@@ -5,6 +5,8 @@ rm -rf maven-test
 git clone https://Texasjake95@github.com/Texasjake95/maven-repo.git ./maven-test -v
 ./gradlew build uploadArchives
 cd ./maven-test
+git remote rm origin
+git remote add origin https://Texasjake95:${GH_TOKEN}@github.com/Texasjake95/maven-repo.git
 git add .
 git commit -m "Travis-CI Build Push"
 git push 
